b6ce1ff6605c3caf2eaccc1caf9e55f69644f019
webtoolbx <exrot1007@naver.com>
2023-12-02 16:19:37 +0900

    update for js/css

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+const repository = "tool";
+
 const nextConfig = {
     reactStrictMode: true,
     output: 'export',
-    basePath: '/tool'
+    assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+    trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
 }
 
 module.exports = nextConfig
