4cd946605ae75abdf3cc531c526451a78a45408f
root <31607926+ha-root@users.noreply.github.com>
2024-02-01 22:01:54 +0900

    init

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+const repository = "tool";
+
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'export',
+    assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+    trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+}
+
+module.exports = nextConfig
---
+++ src/app/page.js
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import { prefix } from "../../conifg/config";
+
+export default function Home() {
+  return (
+      <>
+        <header className="bg-dark py-5">
+          <div className="container px-4 px-lg-5 my-5">
+            <div className="text-center text-white">
+              <h1 className="display-4 fw-bolder">Web Tool Box</h1>
+              <p className="lead fw-normal text-white-50 mb-0">Catalog</p>
+            </div>
+          </div>
+        </header>
+        <section className="py-5">
+          <div className="container px-4 px-lg-5 mt-5">
+            <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+              <div className="col mb-5">
+                <div className="card h-100">
+                  <div className="card-body p-4">
+                    <div className="text-center">
+                      <h5 className="fw-bolder">Mybatis / Colon</h5>
+                      Mybatis 바인딩과 Colon 변경
+                    </div>
+                  </div>
+                  <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <div className="text-center"><a className="btn btn-outline-dark mt-auto" href={`${prefix}/mbc`}>View</a></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+  )
+}
