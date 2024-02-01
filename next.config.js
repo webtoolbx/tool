61eb448871d98cc767c6a5b33b60824cf1a0488c
root <31607926+ha-root@users.noreply.github.com>
2024-02-01 22:06:08 +0900

    delete export

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const repository = "tool";
 
 const nextConfig = {
     reactStrictMode: true,
-    output: 'export',
     assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
     trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
 }
